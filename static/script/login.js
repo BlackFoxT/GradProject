19dce8ef3da3bc3bb077c8d8e554c640872a1749
BlackFoxT <hidayetakturk61@gmail.com>
2024-12-06 15:12:09 +0300

    Organizing html, css and js file locations

---
+++ static/script/login.js
@@ -0,0 +1,22 @@
+const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+document.getElementById('login-next-button').addEventListener('click', function () {
+    let loginMailInput = document.getElementById('login-email');
+    let loginMailValue = loginMailInput.value;
+
+    if (emailRegex.test(loginMailValue)) {
+        document.getElementById('login-next-button').style.display = "none";
+        document.getElementById('login-submit-button').style.display = "block";
+        document.getElementById('login-mail-input').style.display = "none";
+        document.getElementById('login-password-input').style.display = "block";
+
+        // Bu kısmın ne olduğunu anlamadım artık htmlde form ile checking yapılıyor
+    }
+    else {
+
+        var toast = new bootstrap.Toast(document.getElementById('emailToastforLogin'));
+        toast.show();
+
+        loginMailInput.focus(); // Bunun da ne olduğunu bilmiyorum
+    }
+});
