85e02754afc0abbe6c4e7e59f3ab81c34d298a94
BlackFoxT <hidayetakturk61@gmail.com>
2025-04-12 10:57:03 +0300

    Resolving profile problem

--- static/script/profile.js
+++ static/script/profile.js
@@ -1,11 +1,9 @@
 const toggleSidebarButton = document.getElementById("toggle-sidebar");
 const sidebar = document.getElementById("sidebar");
 
-  if (sidebar) {
-    sidebar.classList.add("show");
-    if (toggleSidebarButton && sidebar) {
-      toggleSidebarButton.addEventListener("click", function () {
-        sidebar.classList.toggle("show");
-      });
-    }
+  sidebar.classList.add("show");
+  if (toggleSidebarButton && sidebar) {
+    toggleSidebarButton.addEventListener("click", function () {
+      sidebar.classList.toggle("show");
+    });
   }
