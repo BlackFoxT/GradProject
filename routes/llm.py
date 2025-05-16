4585464e751a67b57aa79f30335397078bb8dc85
Zubair Rahimi <zubair.rahimi1683@gmail.com>
2025-05-16 13:48:46 +0300

    last

--- routes/llm.py
+++ routes/llm.py
@@ -1,7 +1,7 @@
 from langchain_community.llms.ollama import Ollama
 
 # Initialize Ollama model
-llm = Ollama(model="deepseek-r1:7b")
+llm = Ollama(model="deepseek-r1:1.5b")
 
 # Invoke the model:
 def invoke_message(message):
